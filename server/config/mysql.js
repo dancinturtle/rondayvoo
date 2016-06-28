291a249459814894846c3041f585365f49a37690
dancinturtle <amy.giver@gmail.com>
2016-06-27 19:07:16 -0700

    revert

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var fs = require('fs');
 var path = require('path');
 var con = mysql.createConnection({
-      host: "ubuntu",
+      host: "localhost",
       port: 3306,
       user: "root",
       password: "root",
