59e1772debe5d0e05fc9b5afd83a386468e3e88d
dancinturtle <amy.giver@gmail.com>
2016-06-29 13:09:05 -0700

    still the same

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,17 +7,17 @@ module.exports = function(app){
   app.get('/users/getall', function(req, res){
     console.log("Getting stuff");
     users.index(req, res)
-  });
+  })
 
   app.post('/adduser', function(req, res){
     console.log("Routes,", req);
     users.create(req, res);
-  });
+  })
 
   app.post('/update_user_region', function(req, res){
     console.log("Routes to update user region,", req);
     users.update_region(req, res);
-  });
+  })
 
   app.post('/finduser', function(req, res){
     console.log("Routes to find user", req.body);
